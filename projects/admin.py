83b089249f4cd43c503aab2b71d8ff28cefa64fb
Hassani <hassanisnr@gmail.com>
2019-04-21 10:10:43 -0400

    Added to projects admin page

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PorjectModel
+
+
+@admin.register(PorjectModel)
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "description",
+        "github",
+        "url",
+        "dateAdded",
+        "lastModified",
+    )
+    search_fields = ("id", "title", "dateAdded")
+    list_per_page = 10
