80ffc1cc2bcecc8316178af0450dbc238d971570
Hassani <hassanisnr@gmail.com>
2019-04-29 00:04:05 -0400

    Adding favicon & fixing navbar

--- pages/static/js/index.js
+++ pages/static/js/index.js
@@ -1,29 +1,57 @@
+let brandClicked = false;
+let scrollPos = 0;
+let scrollDir = "";
+
 particlesJS.load('particles-js', 'static/particles.json', function () {
     console.log('particles.js config loaded');
 });
+
 
 (function ($) {
     $(document).ready(function () {
         $(".navbar").hide();
         $(function () {
             $(window).scroll(function () {
-                $('.navbar').removeClass('d-none');
-                if ($(this).scrollTop() > 500) {
-                    $('.navbar').fadeIn();
-                } else {
-                    $('.navbar').fadeOut();
+                if (scrollDir === 'DOWN' && brandClicked === false) {
+                    $('.navbar').removeClass('d-none');
+                    if ($(this).scrollTop() > 500) {
+                        $('.navbar').fadeIn();
+                    } else {
+                        $('.navbar').fadeOut();
+                    }
+                } else if (scrollDir === 'UP' && brandClicked === false) {
+                    if ($(this).scrollTop() < 500) {
+                        $('.navbar').fadeOut();
+                    }
                 }
             });
         });
     });
 }(jQuery));
 
+
+window.addEventListener('scroll', function(){
+    if ((document.body.getBoundingClientRect()).top > scrollPos)
+        scrollDir = 'UP'
+    else
+        scrollDir = 'DOWN'
+    scrollPos = (document.body.getBoundingClientRect()).top;
+});
+document.getElementById('nav-brand').addEventListener('click', () => {
+    brandClicked = true;
+    $('.navbar').fadeOut();
+    $('html, body').animate({
+        scrollTop: 0
+    }, 100);
+    setTimeout(() => {
+        brandClicked = false;
+    }, 100);
+});
 document.getElementById('nav-skills').addEventListener('click', () => {
     $('html, body').animate({
         scrollTop: ($('#skills').offset().top)
     }, 100);
 });
-
 document.getElementById('nav-projects').addEventListener('click', () => {
     $('html, body').animate({
         scrollTop: ($('#projects').offset().top)
@@ -39,6 +67,7 @@ document.getElementById('nav-contactme').addEventListener('click', () => {
         scrollTop: ($('#contact').offset().top)
     }, 100);
 });
+
 
 setTimeout(function() {
     $('#message').fadeOut('slow')
